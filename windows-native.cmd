c6af095c91958b29ce51c26b84d04bd79670a225
gskeleton <222417345+gskeleton@users.noreply.github.com>
2026-01-08 11:12:55 -0800

    Update windows-native.cmd

--- windows-native.cmd
+++ windows-native.cmd
@@ -3,7 +3,9 @@ cd /d "%~dp0"
 set "PATH=C:\libdog;%PATH%"
 if not exist "%~dp0watchdogs.win" (
     echo ERROR: watchdogs.win not found in "%~dp0"
+    pause
     exit /b 1
 )
 "%~dp0watchdogs.win" %*
+pause
 exit /b %ERRORLEVEL%
